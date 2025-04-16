3c12622761ec5267ed0c4b5cba1f2424ac599894
Nicolas <nicolascarreira2005@gmail.com>
2025-04-16 15:08:12 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/Project-Banestes/" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
